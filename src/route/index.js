0546d80bfe5170d99663732eda0736af29f87825
Sensation02 <vasia1415@gmail.com>
2023-07-15 16:32:01 +0300

    2-product

--- src/route/index.js
+++ src/route/index.js
@@ -51,6 +51,46 @@ class User {
     }
   }
 }
+
+class Product {
+  constructor(name, price, description) {
+    this.id = Math.floor(Math.random() * 100000)
+    this.createDate = new Date().toISOString()
+    this.name = name
+    this.price = price
+    this.description = description
+  }
+  static #list = []
+  static getList = () => this.#list
+  static add = (product) => this.#list.push(product)
+  static getById = (id) =>
+    this.#list.find((product) => product.id === id)
+  static updateById = (id, data) => {
+    const product = this.getById(id)
+    if (product) {
+      this.update(product, data)
+      return true
+    } else {
+      return false
+    }
+  }
+  static update = (name, { product }) => {
+    if (name) {
+      product.name = name
+    }
+  }
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (product) => product.id === id,
+    )
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+}
 // ================================================================
 
 router.get('/', function (req, res) {
@@ -63,6 +103,13 @@ router.get('/', function (req, res) {
         isEmpty: list.length === 0,
       },
     },
+  })
+})
+
+router.get('/product-create', function (req, res) {
+  const list = Product.getList()
+  res.render('product-create', {
+    style: 'product-create',
   })
 })
 
@@ -81,6 +128,28 @@ router.post('/user-create', function (req, res) {
   })
 })
 
+router.post('/product-create', function (req, res) {
+  const { name, price, description } = req.body
+  const product = new Product(name, price, description)
+  Product.add(product)
+
+  if (Product.getById(product.id)) {
+    result = true
+  } else {
+    result = false
+  }
+
+  res.render('alert', {
+    style: 'alert',
+    title: result
+      ? 'Успішне виконання дії'
+      : 'Не успішне виконання дії',
+    info: result
+      ? 'Товар успішно був доданий'
+      : 'Товар не був доданий',
+  })
+})
+
 // ================================================================
 
 router.get('/user-delete', function (req, res) {
@@ -93,6 +162,21 @@ router.get('/user-delete', function (req, res) {
   })
 })
 
+router.get('/product-list', function (req, res) {
+  // отримуємо список товарів та виводимо їх на сторінку
+  const list = Product.getList()
+  console.log(list) // виводимо список товарів в консоль (для перевірки)
+  res.render('product-list', {
+    style: 'product-list',
+    data: {
+      products: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
+  })
+})
+
 // ================================================================
 
 router.post('/user-update', function (req, res) {
@@ -101,7 +185,8 @@ router.post('/user-update', function (req, res) {
   const user = User.getById(+id)
 
   if (user.verifyPassword(password)) {
-    result = User.update(user, { email })
+    User.update(user, { email })
+    return (result = true)
   }
 
   res.render('success-info', {
@@ -112,6 +197,69 @@ router.post('/user-update', function (req, res) {
   })
 })
 
+router.get('/product-edit', function (req, res) {
+  const { id } = req.query
+  const product = Product.getById(+id)
+  if (product) {
+    return res.render('product-edit', {
+      style: 'product-edit',
+      data: {
+        name: product.name,
+        price: product.price,
+        id: product.id,
+        description: product.description,
+      },
+    })
+  } else {
+    return res.render('alert', {
+      style: 'alert',
+      title: 'Не успішне виконання дії',
+      info: 'Товар не знайдено',
+    })
+  }
+})
+
 // ================================================================
+
+router.post('/product-edit', function (req, res) {
+  const { id, name, price, description } = req.body
+  const product = Product.updateById(id, {
+    name,
+    price,
+    description,
+  })
+  if (product) {
+    res.render('alert', {
+      style: 'alert',
+      title: 'Успішне виконання дії',
+      info: 'Інформація про товар оновлена',
+    })
+  } else {
+    res.render('alert', {
+      style: 'alert',
+      title: 'Не успішне виконання дії',
+      info: 'Помилка при оновленні інформації про товар',
+    })
+  }
+})
+
+router.get('/product-delete', function (req, res) {
+  const { id } = req.query
+  Product.deleteById(+id)
+  if (Product.getById(+id)) {
+    result = false
+  } else {
+    result = true
+  }
+  res.render('alert', {
+    style: 'alert',
+    title: result
+      ? 'Успішне виконання дії'
+      : 'Не успішне виконання дії',
+    info: result
+      ? 'Товар успішно видалений'
+      : 'Товар не був видалений',
+  })
+})
 // Підключаємо роутер до бек-енду
 module.exports = router
